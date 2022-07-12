a62f202ec50c57f3981c866c78bceb0011435ba5
daeldero <108710566+daeldero@users.noreply.github.com>
2022-07-12 17:04:20 +0900

    Create array_test.cpp

---
+++ ordered_list/array_test.cpp
@@ -0,0 +1,29 @@
+#include "array.h"
+#include "../test.h"
+
+#include <iostream>
+
+void PrintArray(const Array<int>& arr) {
+	std::cout << "Size = " << arr.size() << ": ";
+	for (int i = 0; i < arr.size(); ++i) std::cout << arr[i] << ' ';
+	std::cout << '\n';
+}
+
+void TestArray() {
+	Array<int> arr;
+	for (int i = 1; i <= 5; ++i) {
+		arr.PushBack(i);
+		PrintArray(arr);
+	}
+	for (int i = 6; i <= 10; ++i) {
+		arr.PushFront(i);
+		PrintArray(arr);
+	}
+
+	int idx = 0;
+	while (!arr.empty()) {
+		if (idx % 2 == 0) arr.PopBack();
+		else arr.PopFront();
+		PrintArray(arr);
+	}
+}
