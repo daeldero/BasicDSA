b5298486f676a11adebbd8537186edbef3664cfd
daeldero <sbtreetoy@gmail.com>
2022-07-15 01:40:03 +0900

    Create list_node.h

---
+++ ordered_list/list_node.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template<typename T, bool Double = false>
+struct Node {
+	Node(const T& data = T()) : data_{ data } {};
+
+	T data_;
+	Node* next_{ nullptr };
+};
+
+template<typename T>
+struct Node<T, true> {
+	Node(const T& data = T()) : data_{ data } {};
+
+	T data_;
+	Node* prev_{ nullptr };
+	Node* next_{ nullptr };
+};
